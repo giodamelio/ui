2be4619cc626cbb9108621b4c5940443a4cc6dbc
Gio d'Amelio <giodamelio@gmail.com>
2014-11-27 21:30:17 -0800

    Switch to termbox

--- ui.go
+++ ui.go
@@ -1,12 +1,9 @@
 package ui
 
 import (
-	"fmt"
 	"os"
-	"os/signal"
-	"syscall"
 
-	"github.com/giodamelio/ui/syscalls"
+	"github.com/nsf/termbox-go"
 )
 
 // UI type
@@ -14,50 +11,31 @@ type UI struct{}
 
 // Run the app
 func (ui *UI) Run() {
-	// Listen for interrupt so we can exit cleanly
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-c
-		syscalls.MakeSane()
-		os.Exit(0)
-	}()
+	// Setup termbox
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
 
-	// Prep the terminal
-	syscalls.MakeRaw()
+	// Put the terminal in raw mode
+	termbox.SetInputMode(termbox.InputEsc | termbox.InputMouse)
 
 	// Run the main loop
-	mainLoop()
-}
-
-// The main loop
-func mainLoop() {
-	// Read keyboard input
-	keys := make(chan Key)
-	go readKeyboard(keys)
-
-	for key := range keys {
-		fmt.Println(key)
-	}
-}
-
-// Read keypresses from stdin
-func readKeyboard(keys chan Key) {
-	// Buffer for the key
-	var b = make([]byte, 3)
-
-	// Loop forever reading keys
 	for {
-		// Read key from stdin
-		os.Stdin.Read(b)
-
-		// Send the key over the channel
-		key := GetKey(b)
-		keys <- key
-
-		// Empty the buffer
-		b[0] = 0
-		b[1] = 0
-		b[2] = 0
+		event := termbox.PollEvent()
+		switch event.Type {
+		case termbox.EventKey:
+			// If C-c is pressed, cleanup the terminal and exit
+			if event.Key == termbox.KeyCtrlC {
+				cleanExit()
+			}
+		}
 	}
+}
+
+// Exit cleanly
+func cleanExit() {
+	termbox.Close()
+	os.Exit(0)
 }
